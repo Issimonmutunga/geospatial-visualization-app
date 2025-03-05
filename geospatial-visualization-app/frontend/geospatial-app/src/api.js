1527a20293095f77e7007bd51db2de3afb7c857f
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-03-05 21:11:05 +0300

    Edited the base url to point to the backend

--- geospatial-visualization-app/frontend/geospatial-app/src/api.js
+++ geospatial-visualization-app/frontend/geospatial-app/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5000", // Adjust if necessary
+  baseURL: "http://localhost:8000", // Adjust if necessary
   timeout: 10000,
 });
 

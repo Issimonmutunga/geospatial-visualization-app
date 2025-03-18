d7266fbae572a864c274789ec8d970633437670c
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-03-18 18:15:44 +0300

    For shows

--- geospatial-visualization-app/frontend/geospatial-app/src/components/MapViewer.js
+++ geospatial-visualization-app/frontend/geospatial-app/src/components/MapViewer.js
@@ -1,14 +1,15 @@
-//Renders Interactive Map
+// Renders Interactive Map
 import React, { useState, useEffect } from "react";
-import { MapContainer, TileLayer, GeoJSON } from "react-leaflet";
+import { MapContainer, TileLayer, GeoJSON, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import axios from "../api"; // Centralized API requests
+import L from "leaflet";
 
 const MapViewer = () => {
   const [geoData, setGeoData] = useState(null);
 
   useEffect(() => {
-    axios.get("/api/views") // Fetch GeoJSON data from backend.Pls edit 3/18/2025
+    axios.get("/api/visualization")
       .then((response) => {
         setGeoData(response.data);
       })
@@ -17,10 +18,26 @@ const MapViewer = () => {
       });
   }, []);
 
+  function FitBounds({ geoData }) {
+    const map = useMap();
+    useEffect(() => {
+      if (geoData) {
+        const bounds = L.geoJSON(geoData).getBounds();
+        map.fitBounds(bounds);
+      }
+    }, [geoData, map]);
+    return null;
+  }
+
   return (
-    <MapContainer center={[-1.286389,36.817223]} zoom={15} style={{ height: "500px", width: "100%" }}>
+    <MapContainer style={{ height: "500px", width: "100%" }} zoom={2} center={[0, 0]}>
       <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-      {geoData && <GeoJSON data={geoData} />}
+      {geoData && (
+        <>
+          <GeoJSON data={geoData} />
+          <FitBounds geoData={geoData} />
+        </>
+      )}
     </MapContainer>
   );
 };

acf5ae4b31ab3b99920cd2f67ee97d6599e9d003
Simon Mutunga <107920763+Issimonmutunga@users.noreply.github.com>
2025-03-26 08:46:17 +0300

    Panel addition to the mix

--- geospatial-visualization-app/frontend/geospatial-app/src/App.js
+++ geospatial-visualization-app/frontend/geospatial-app/src/App.js
@@ -6,13 +6,15 @@ import React from "react";
 import MapViewer from "./components/MapViewer";
 import FileUploader from "./components/FileUploader";
 import PredictionPanel from "./components/PredictionPanel";
+import Panel from './components/panel';
 
 function App() {
   return (
     <div>
-      <h2>Map</h2>
-      <FileUploader />
+      <h2>----</h2>
       <MapViewer />
+      <Panel/>
+      <FileUploader />  
       <PredictionPanel />
     </div>
   );
